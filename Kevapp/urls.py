6920e2c45d516e9fe90eabfe3916751f4e684a46
KevinKoyo <koyokevin10@gmail.com>
2024-11-11 23:25:21 +0300

    third

--- Kevapp/urls.py
+++ Kevapp/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns=[
     path('',views.home,name='my_home'),
+    path('about',views.about,name='my_about'),
+    path('login',views.login,name='my_login'),
+    path('offers',views.service,name='my_services'),
+    path('contact',views.contact,name='my_contact'),
+    path('blog',views.blog,name='my_blog'),
+    path('new',views.new,name='my_new'),
 
 
 ]
--- Kevapp/views.py
+++ Kevapp/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def service(request):
+    return render(request, 'offers.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def new(request):
+    return  render(request,'new.html')
